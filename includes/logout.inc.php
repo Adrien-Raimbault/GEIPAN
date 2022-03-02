6f7b78a4b0495658ba3f9bf643df1ec9e0e81f70
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-03-02 15:04:14 +0100

    modif mineure
    ajout de saut de ligne

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -2,4 +2,4 @@
 $_SESSION['login'] = false;
 session_unset();
 session_destroy();
-echo "<script>document.location.replace('http://localhost:8888/GEIPAN/index.php?page=home');</script>";   
+echo "<script>document.location.replace('http://localhost:8888/GEIPAN/index.php?page=home');</script>";
