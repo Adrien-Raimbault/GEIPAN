05fb494fc7a02a038133827ee94edd48135d6a8f
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-03-02 15:03:03 +0100

    Ajout options menu de navigation
    Implémente notion de login ou non

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,15 @@
     <nav role="navigation">
         <ul>
             <li><a href="index.php?page=home">Accueil</a></li>
+            <?php
+            if(isset($_SESSION['login']) && $_SESSION['login'] === true){
+                echo "<li><a href=\"index.php?page=logout\">Se déconnecter</a></li>";
+                echo "<li><a href=\"index.php?page=account\">Mon compte</a></li>";
+            } else {
+                echo "<li><a href=\"index.php?page=login\">Se connecter</a></li>";
+                echo "<li><a href=\"index.php?page=inscription\">S'inscrire</a></li>";
+            }
+            ?>
         </ul>
     </nav>
 </header>
